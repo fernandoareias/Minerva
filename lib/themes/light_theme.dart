2bb3924407ef62b0ea8c5a6184330b74cc4d27bc
Fernando <nando.calheirosx@gmail.com>
2021-07-24 15:16:31 -0300

    Initial theme

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+
+LoginTheme loginTheme() {
+  return LoginTheme(
+    cardTheme: CardTheme(
+      color: Colors.white.withOpacity(0.9),
+    ),
+    // primaryColor: Color(),
+  );
+}
