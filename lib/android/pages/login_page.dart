afd47abe8ca5cafab072dfcf9c03bc2654914fea
Fernando <nando.calheirosx@gmail.com>
2021-07-24 15:15:59 -0300

    Completed the temporary login page

---
+++ lib/android/pages/login_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:minerva/android/pages/home_page.dart';
+import 'package:minerva/android/widgets/login_page/button_widget.dart';
+import 'package:minerva/android/widgets/login_page/input_widget.dart';
+
+const users = const {'test@test.com': 'test', 'test2@test.com': 'test123'};
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  // Simuled delay connection with BD
+  Duration get login_duration => Duration(milliseconds: 2000);
+  TextStyle get style => TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFFA2E7B6),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: 60,
+            ),
+            child: Container(
+              width: 90,
+              height: 90,
+              child: Image(image: AssetImage("assets/login_page/logo.png")),
+            ),
+          ),
+          Column(
+            children: [
+              // Input user email
+              inputWidget(
+                bottom: 20.0,
+                label: "Email ID",
+                icon: Icons.email,
+              ),
+
+              // Input user password
+              inputWidget(
+                bottom: 20.0,
+                obscureText: true,
+                label: "Password",
+                icon: Icons.lock,
+              ),
+
+              // Button
+              button(
+                context: context,
+                page: HomePage(),
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 20.0,
+                ),
+                label: "Login",
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image(
+              image: AssetImage("assets/login_page/background.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
