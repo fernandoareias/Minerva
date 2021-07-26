70aa43ed5d303ef7e430508547bbd2b26b36054f
Fernando <nando.calheirosx@gmail.com>
2021-07-26 17:44:15 -0300

    Include Stack

--- lib/android/pages/login_page.dart
+++ lib/android/pages/login_page.dart
@@ -16,24 +16,38 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xFFA2E7B6),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // Logo Project
-          Padding(
-            padding: EdgeInsets.only(
-              top: 60,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image(
+              image: AssetImage("assets/login_page/background.png"),
             ),
-            child: Container(
-              width: 90,
-              height: 90,
-              child: Image(image: AssetImage("assets/login_page/logo.png")),
+          ),
+          // Logo Project
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 60,
+              ),
+              child: Container(
+                width: 90,
+                height: 90,
+                child: Image(image: AssetImage("assets/login_page/logo.png")),
+              ),
             ),
           ),
 
           // Login Form
           Column(
             children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 180,
+                ),
+              ),
               // Input user email
               inputWidget(
                 bottom: 20.0,
@@ -63,12 +77,6 @@ class LoginPage extends StatelessWidget {
           ),
 
           // Image Footer
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Image(
-              image: AssetImage("assets/login_page/background.png"),
-            ),
-          ),
         ],
       ),
     );
