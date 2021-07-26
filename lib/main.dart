4b6d13f2cd6c3fddff1dd2e3419d4946d4ff310a
Fernando <nando.calheirosx@gmail.com>
2021-07-25 21:04:32 -0300

    null

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'android/pages/login_page.dart';
+import 'android/pages/location_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LocationPage(),
     );
   }
 }
